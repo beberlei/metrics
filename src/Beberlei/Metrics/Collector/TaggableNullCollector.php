43916c3c4a8af6dc278f6da2aa31e6df2054e271
Jan Deschuttere <jan.deschuttere@gmail.com>
2018-01-25 17:31:55 +0100

    Adding beiberlei disclaimer
    making it mergeable by a touch of a button

--- src/Beberlei/Metrics/Collector/TaggableNullCollector.php
+++ src/Beberlei/Metrics/Collector/TaggableNullCollector.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * Beberlei Metrics.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.txt.
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to kontakt@beberlei.de so I can send you a copy immediately.
+ */
 namespace Beberlei\Metrics\Collector;
 
 class TaggableNullCollector implements Collector, InlineTaggableGaugeableCollector
