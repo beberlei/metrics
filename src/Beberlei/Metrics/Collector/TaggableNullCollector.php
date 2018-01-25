47fe66e56b84836ea6c3fb2e28d1fc4ac39777b5
Jan Deschuttere <jan.deschuttere@gmail.com>
2018-01-25 17:29:09 +0100

    Clean the docblocks

--- src/Beberlei/Metrics/Collector/TaggableNullCollector.php
+++ src/Beberlei/Metrics/Collector/TaggableNullCollector.php
@@ -5,12 +5,7 @@ namespace Beberlei\Metrics\Collector;
 class TaggableNullCollector implements Collector, InlineTaggableGaugeableCollector
 {
     /**
-     * Updates a counter by some arbitrary amount.
-     *
-     * @param string $variable
-     * @param int $value The amount to increment the counter by
-     * @param array $tags Tags to be attached to the metric
-     * @return
+     * {@inheritdoc}
      */
     public function measure($variable, $value, $tags = array())
     {
@@ -18,10 +13,7 @@ class TaggableNullCollector implements Collector, InlineTaggableGaugeableCollect
     }
 
     /**
-     * Increments a counter.
-     *
-     * @param string $variable
-     * @param array $tags Tags to be attached to the metric
+     * {@inheritdoc}
      */
     public function increment($variable, $tags = array())
     {
@@ -29,10 +21,7 @@ class TaggableNullCollector implements Collector, InlineTaggableGaugeableCollect
     }
 
     /**
-     * Decrements a counter.
-     *
-     * @param string $variable
-     * @param array $tags Tags to be attached to the metric
+     * {@inheritdoc}
      */
     public function decrement($variable, $tags = array())
     {
@@ -40,11 +29,7 @@ class TaggableNullCollector implements Collector, InlineTaggableGaugeableCollect
     }
 
     /**
-     * Records a timing.
-     *
-     * @param string $variable
-     * @param int $time The duration of the timing in milliseconds
-     * @param array $tags Tags to be attached to the metric
+     * {@inheritdoc}
      */
     public function timing($variable, $time, $tags = array())
     {
@@ -52,11 +37,7 @@ class TaggableNullCollector implements Collector, InlineTaggableGaugeableCollect
     }
 
     /**
-     * Updates a gauge by an arbitrary amount.
-     *
-     * @param string $variable
-     * @param int $value
-     * @param array $tags Tags to be attached to the metric
+     * {@inheritdoc}
      */
     public function gauge($variable, $value, $tags = array())
     {
@@ -64,7 +45,7 @@ class TaggableNullCollector implements Collector, InlineTaggableGaugeableCollect
     }
 
     /**
-     * Sends the metrics to the adapter backend.
+     * {@inheritdoc}
      */
     public function flush()
     {
